b5c907416e707c5240e9bcb3e9b586271f063c03
Katie Reed <katharynreedpierce@gmail.com>
2017-03-17 11:11:59 -0700

    complete interface and backend

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -1,17 +1,9 @@
 var Doctor = require('./../js/doc.js').doctorModule;
 
-var displayDoctors = function(medicalIssue, doctors) {
-  for (var i=0; i<doctors.length; i++){
-    $('#showDoctors').append(
-      "<li>" + doctors[i] + "</li>"
-    );
-  }
-};
-
-$(document).ready(function(){
-  var currentDoctorObject = new Doctor();
-  $('#medicalIssue').click(function() {
-    $('#medicalInput').val("");
-    currentDoctorObject.getDoctors(medicalIssue, doctors);
+$(document).ready(function() {
+  $('#userSubmit').click(function() {
+    var medicalIssue = $('#userInput').val();
+    var newDoctor = new Doctor(medicalIssue);
+    var result = newDoctor.findDoctors(medicalIssue);
   });
 });
--- js/doc.js
+++ js/doc.js
@@ -1,20 +1,23 @@
-var apiKey = require('./../.env').apiKey;
+function Doctor() {
+}
 
-Doctor = function(){
-};
+Doctor.prototype.findDoctors = function(userInput) {
+    
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + userInput + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=4e8845222bc6a858d55f09ca29dc833a')
+     .then(function(result) {
+        if (result.data.length > 0) {
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
-  var doctors = [];
-
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(response) {
-      for(var i = 0; i < response.doctors.length; i++){
-        doctors.push(response.doctors[i].name);
-      }
-    })
-   .fail(function(error){
+          for (var i = 0; i < result.data.length; i++) {
+            $('#showDoctors').append('<li>' + result.data[i].profile.first_name + ' ' +  result.data[i].profile.last_name + ', ' + result.data[i].profile.title + '</li>');
+            console.log(result.data[i].profile.first_name);
+          }
+        } else {
+          $('#showDoctors').append('There are no specialists in Portland for this condition.');
+        }
+      })
+    .fail(function(error){
       console.log("fail");
     });
-};
+  };
 
-exports.doctorModule = Doctor;
+  exports.doctorModule = Doctor;
