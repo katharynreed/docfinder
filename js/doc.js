a33a093fa56b18a2e6f2f378c447c2a1cbeaf643
Katie Reed <katharynreedpierce@gmail.com>
2017-03-17 13:31:34 -0700

    add error message to failed API call

--- js/doc.js
+++ js/doc.js
@@ -12,11 +12,11 @@ Doctor.prototype.findDoctors = function(userInput) {
           $('#showDoctors').append('<div id="doctorInfo" class="media page-header"><div class="media-left"><img id="doctorImage" class="media-object" src="' + result.data[i].profile.image_url + '" alt=""></div><div class="media-body"><h4 class="media-heading">' + result.data[i].profile.first_name + ' ' +  result.data[i].profile.last_name + ', ' + result.data[i].profile.title +  '</h4>' + result.data[i].profile.bio + '</div>');
         }
       } else {
-        $('#showDoctors').append('<h3 id="noShow">There are no specialists in Portland for this condition.</h3>');
+        $('#showDoctors').text('<h3 id="noShow">There are no specialists in Portland for this condition.</h3>');
       }
     })
   .fail(function(error){
-    console.log("fail");
+    $('#showDoctors').text('<h3>Error. Please try again.</h3>');
   });
 };
 
