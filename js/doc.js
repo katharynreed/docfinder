406f30b2f7cb56fb6c15b511e1061e6ba0af3615
Katie Reed <katharynreedpierce@gmail.com>
2017-03-17 13:10:04 -0700

    style content and complete styling of landing page using sass

--- js/doc.js
+++ js/doc.js
@@ -9,7 +9,7 @@ Doctor.prototype.findDoctors = function(userInput) {
         if (result.data.length > 0) {
 
           for (var i = 0; i < result.data.length; i++) {
-            $('#showDoctors').append('<h2>' + result.data[i].profile.first_name + ' ' +  result.data[i].profile.last_name + ', ' + result.data[i].profile.title +  '</h2><img id="doctorImage" src="' + result.data[i].profile.image_url + '" alt=""></h3>' + '<blockquote>' + result.data[i].profile.bio + '</blockquote>');
+            $('#showDoctors').append('<div id="doctorInfo" class="media page-header"><div class="media-left"><img id="doctorImage" class="media-object" src="' + result.data[i].profile.image_url + '" alt=""></div><div class="media-body"><h4 class="media-heading">' + result.data[i].profile.first_name + ' ' +  result.data[i].profile.last_name + ', ' + result.data[i].profile.title +  '</h4>' + result.data[i].profile.bio + '</div>');
           }
         } else {
           $('#showDoctors').append('<h3 id="noShow">There are no specialists in Portland for this condition.</h3>');
