8a5fdf27ac6cf724044d12c6bef6fb203c11845e
Katie Reed <katharynreedpierce@gmail.com>
2017-03-17 09:10:42 -0700

    creat initial js file

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -1,9 +1,17 @@
 var Doctor = require('./../js/doc.js').doctorModule;
 
-var displayDoctors = function(medicalIssue) {
+var displayDoctors = function(medicalIssue, doctors) {
   for (var i=0; i<doctors.length; i++){
-    $('#doctors').append(
+    $('#showDoctors').append(
       "<li>" + doctors[i] + "</li>"
     );
   }
 };
+
+$(document).ready(function(){
+  var currentDoctorObject = new Doctor();
+  $('#medicalIssue').click(function() {
+    $('#medicalInput').val("");
+    currentDoctorObject.getDoctors(medicalIssue, doctors);
+  });
+});
--- js/doc.js
+++ js/doc.js
@@ -4,9 +4,13 @@ Doctor = function(){
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
+  var doctors = [];
+
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+   .then(function(response) {
+      for(var i = 0; i < response.doctors.length; i++){
+        doctors.push(response.doctors[i].name);
+      }
     })
    .fail(function(error){
       console.log("fail");
